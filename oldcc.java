78a4bf591f045495eb06864c0de551ff20fa5d0e
tom <tom@example.com>
2019-08-29 02:43:46 +0000

    First commit

---
+++ oldcc.java
@@ -0,0 +1,8 @@
+# This change was worked on by jerry - Jerry added a new function called visapayments()
+- Fix the credit card payment problem
+
+patch fix
+some fix
+
+
+changes by jerry to fix some issues - master branch
